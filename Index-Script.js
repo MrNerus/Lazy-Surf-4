49bcb4f1dde3243833f825a38e6463037017b702
unknown <mr.ard.nerus@gmail.com>
2021-01-11 11:35:03 +0545

    first commit

---
+++ Index-Script.js
@@ -0,0 +1,52 @@
+function PopUpArd101() {document.getElementById('Ard101').style.display='block';}
+function openQLYoutube() {document.getElementById("QLYoutube").style.display = "block";}
+function closeQLYoutube() {document.getElementById("QLYoutube").style.display = "none";}
+function openQLGoogle() {document.getElementById("QLGoogle").style.display = "block";}
+function closeQLGoogle() {document.getElementById("QLGoogle").style.display = "none";}
+function openQLIOGames() {document.getElementById("QLIOGames").style.display = "block";}
+function closeQLIOGames() {document.getElementById("QLIOGames").style.display = "none";}
+function openQLWebApps() {document.getElementById("QLWebApps").style.display = "block";}
+function closeQLWebApps() {document.getElementById("QLWebApps").style.display = "none";}
+function openQLResearch() {document.getElementById("QLResearch").style.display = "block";}
+function closeQLResearch() {document.getElementById("QLResearch").style.display = "none";}
+function openQLProxies() {document.getElementById("QLProxies").style.display = "block";}
+function closeQLProxies() {document.getElementById("QLProxies").style.display = "none";}
+/*
+function myFunction() {
+	var x = document.getElementById("Demo");
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+	} else { 
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+*/
+
+function PopUpUnCategorizedWeb() {document.getElementById('UnCategorizedWeb').style.display='block';}
+
+
+function PopUpEducational() {document.getElementById('Educational').style.display='block';}
+
+
+function PopUpSearchIt() {document.getElementById('SearchIt').style.display='block';}
+
+
+function PopUpSomeSlots() {document.getElementById('SomeSlots').style.display='block';}
+
+
+function PopUpTeleFilm() {document.getElementById('PopUpTeleFilm').style.display='block';}
+function PopUpTV() {document.getElementById('PopUpTV').style.display='block';}
+function PopUpKaryakram() {document.getElementById('PopUpKaryakram').style.display='block';}
+function PopUpGeetHaru() {document.getElementById('PopUpGeetHaru').style.display='block';}
+
+
+
+
+
+
+
+
+
+
+
+function Ard() {windows.create({"url": url, "incognito": true});}
